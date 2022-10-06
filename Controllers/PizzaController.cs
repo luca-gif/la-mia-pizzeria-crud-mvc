3362514f92d24a56a2c5ce1232fb0b34b4fb4631
luca-gif <luca.89m@gmail.com>
2022-10-06 14:08:08 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,109 @@
+﻿using la_mia_pizzeria_static.Context;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            using (Restaurant db = new Restaurant())
+            {
+
+                List<Pizza> pizzas = db.ListaPizze.OrderBy(pizza => pizza.Price).ToList<Pizza>();
+
+                return View("Index", pizzas);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Pizza data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            using (Restaurant db = new Restaurant())
+            {
+                Pizza newPizza = new Pizza(); //istanzio una nuova pizza
+                newPizza.Name = data.Name;
+                newPizza.Image = data.Image;
+                newPizza.Description = data.Description;
+                newPizza.Price = data.Price;
+
+                db.ListaPizze.Add(newPizza);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Restaurant db = new Restaurant();
+            Pizza pizzaToEdit = db.ListaPizze.Where(p => p.PizzaId == id).First();
+                
+            return View(pizzaToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Pizza data)
+        {
+            Restaurant db = new Restaurant();
+
+            db.ListaPizze.Update(data);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Detail(int id)
+        {
+            Restaurant db = new Restaurant();
+
+            Pizza PizzaDetail = db.ListaPizze.Where(p => p.PizzaId == id).First();
+
+            if (PizzaDetail == null)
+            {
+                return NotFound("Ciò che stai cercando non è presente nel nostro database.");
+            }
+            else
+            {
+                return View(PizzaDetail);
+            }
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public Pizza()
+        {
+
+        }
+
+        public int PizzaId { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(30, ErrorMessage = "Il nome non può essere più lungo di 30 caratteri")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(255, ErrorMessage = "Il nome non può essere più lungo di 255 caratteri")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Range(0, 100, ErrorMessage = "Scambi con Porsche Panamera?")]
+        public double Price { get; set; }
+
+    }
+}
